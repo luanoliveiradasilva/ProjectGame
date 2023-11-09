2ed114b36bd9fb363465492c5534ce4ac50565dc
= <=>
2023-11-09 20:50:20 +0000

    add drag and drop to player costumize

---
+++ AppGame/Assets/Scripts/Game/DragAndDropObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropObject : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+      rectTransform = GetComponent<RectTransform>();  
+      canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 0.6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
