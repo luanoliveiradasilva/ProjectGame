48b6e8a7ec29404b30693d78320984fa011e37d1
= <=>
2023-11-13 22:18:44 +0000

    Add scena panel

--- AppGame/Assets/Scripts/UI/AnimationPanel.cs
+++ AppGame/Assets/Scripts/UI/AnimationPanel.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationPanel : MonoBehaviour
 {
@@ -9,24 +11,50 @@ public class AnimationPanel : MonoBehaviour
     [Header("Time the animation")]
     [SerializeField] private float waitAnimation = 1.25f;
 
+    [Header("Buttons")]
+    [SerializeField] private Button showServer;
+    [SerializeField] private Button showCena;
+
+
     [Header("Panels")]
     [Tooltip("Player login Panel")]
     [SerializeField] private GameObject LoginPanel;
 
-    [Tooltip("Server Panel")]
+    [Tooltip("Paineis")]
     [SerializeField] private GameObject serverPanel;
 
+    [SerializeField] private GameObject cenaPanel;
+
     private readonly int HideButton = Animator.StringToHash("Show");
+
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        OnUpPanel();
+    }
+
     public void OnUpPanel()
+    {
+        showServer.onClick.AddListener(ShowServer);
+
+        showCena.onClick.AddListener(ShowCena);
+    }
+
+    private void ShowServer()
     {
         animator.SetTrigger(HideButton);
         StartCoroutine("ShowServerPanel");
+    }
+
+    private void ShowCena()
+    {
+        animator.SetTrigger(HideButton);
+        StartCoroutine("ShowCenaPanel");
     }
 
     private IEnumerator ShowServerPanel()
@@ -34,6 +62,13 @@ public class AnimationPanel : MonoBehaviour
         yield return new WaitForSeconds(waitAnimation);
         LoginPanel.SetActive(false);
         serverPanel.SetActive(true);
+    }
+
+    private IEnumerator ShowCenaPanel()
+    {
+        yield return new WaitForSeconds(waitAnimation);
+        LoginPanel.SetActive(false);
+        cenaPanel.SetActive(true);
     }
 
 }
