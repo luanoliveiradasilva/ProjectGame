97fad167e2fd6bd4de6db1ffdba35861c0ba151d
Rui Costa <133767216+Cousta@users.noreply.github.com>
2024-02-07 14:57:37 +0000

    Adjuste screen tutorials (#14)
    Co-authored-by: = <=>

--- AppGame/Assets/Code/Scripts/Game/Tutorial/TutorialScreen.cs
+++ AppGame/Assets/Code/Scripts/Game/Tutorial/TutorialScreen.cs
@@ -2,8 +2,10 @@ using Scripts.Game;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using Screens.FirstScreen;
+using DG.Tweening;
 
-namespace Tutorial
+namespace Screens.FirstScreen
 {
     public class TutorialScreen : MonoBehaviour
     {
@@ -18,17 +20,20 @@ namespace Tutorial
         [SerializeField] private bool isTutotial;
 
         [SerializeField] private Button enableButton;
+
+        private AnimationTutorial animationTutorial;
         public int valueOnTrigger;
 
         private void Awake()
         {
+            animationTutorial = FindObjectOfType<AnimationTutorial>();
             timeGame = FindObjectOfType<TimeGame>();
         }
 
         void Start()
         {
          
-
+            enableButton.transform.localScale = Vector3.zero;
             if (tutorial.CompareTag("Basket Tutorial") || tutorial.name.Equals("Tutorial Screen 3"))
             {
                 isTutotial = true;
@@ -72,7 +77,8 @@ namespace Tutorial
 
         private void InteractableButton()
         {
-            enableButton.interactable = true;
+            
+            enableButton.transform.DOScale(1.0f, 2).SetEase(Ease.OutSine);
         }
     }
 }
